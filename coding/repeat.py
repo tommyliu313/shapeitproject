0cff5ba96ea25980a5b6784bfd1af382b3c68c7f
tommyliu313 <liuchaktin@gmail.com>
2022-09-15 17:07:45 +0800

    20220915 170700

---
+++ coding/repeat.py
@@ -0,0 +1,17 @@
+# for loop
+# for i in range(0,2):
+#
+#
+# while True:
+#
+# try & catch
+# try {} catch(e){}
+# except x as y:
+# case
+# switch(case){
+# 
+# }
+# Validate whether it is correct
+# self.assertTrue()
+# self.assert()
+
