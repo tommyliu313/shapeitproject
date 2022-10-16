50c6717b172f44ec32d48cf116b88861ada9863e
tommyliu313 <liuchaktin@gmail.com>
2022-10-16 19:41:04 +0800

    20221016 1941

---
+++ coding/opencv.py
@@ -0,0 +1,23 @@
+#import items
+import numpy as np
+import cv2
+#load images
+
+img = cv2.imread()
+
+#class
+class object():
+# convert color
+
+    convert = cv2.cvtColor()
+
+
+#window name
+#視窗名稱,變數名稱
+cv2.imshow("",)
+
+
+# close
+
+cv2.destroyAllWindows()
+cv2.waitKey(0)
