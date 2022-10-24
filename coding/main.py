e52c245032f23207ec8ae3d27408d37feec377bb
tommyliu313 <liuchaktin@gmail.com>
2022-10-25 03:27:50 +0800

    20221025 0327

--- coding/main.py
+++ coding/main.py
@@ -1,31 +1,61 @@
-#import item
-import 
+# Use VSCode to have better visual experience
+# opensource module insert SECTION
+# import item (such as numpy, opencv, pytranscriber etc opensource code)
+# for example numpy
+
+import numpy 
+
+# Variable SECTION
+# Where you should define or simplify something
 
 
-# class
-class example():
-#if you want to inherit/ extend the function of another class,
-#just type the name of that class (superclass) into the class (subclass)
-#such as class student(person)
 
-# initial setting
+# class SECTION
+# Be careful your indentation
+ class example():
+# if you want to inherit/ extend the function of another class,
+# just type the name of that class (superclass) into the class (subclass)
+# such as class student (person)
+
+# initial setting (property)
+
+# all CLASSification should be have their initial setting
     def __init__(self):
-        self
-    
+      self
 
+    #function 1 
+    def function1():
+
+    
+    # if you use some features from the opensource code, make sure to state out which your intention and your action,
+    # you can tell your other developers such as follows:
+
+    # we are going to use opencv Drawing Function,
+    # using cv2.rectangle to write rectangle according those parameters that we need to insert in
+
+        cv2.rectangle()
 
     # processing part
+
     # output something
-
-
+       return 
+    
 # testing part
 class testing():
 
 
 # test(self.assert)
-    def 
-    self.assertTrue()
+    def assert():
+        self.assertTrue()
+
+
 #def main():
 
 # if__name__ == '__main__':
-# main()
+#  main()
+
+
+# Referencing
+# When you are one of the developers, make sure to specify
+# where you found the documents and which section you have
+# insert those ideas.
