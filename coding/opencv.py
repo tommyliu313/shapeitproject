cd0f15fa8cb8647a84cd81edf91605551adfb655
tommyliu313 <liuchaktin@gmail.com>
2022-11-04 05:41:30 +0800

    20221104 0541

--- coding/opencv.py
+++ coding/opencv.py
@@ -2,10 +2,17 @@
 import numpy as np
 import cv2
 
+# setting
+yellow = (255,255,0)
+black = (0,0,0)
+window_size = (1080,720,3)
+text = "Testing"
 
 #load images
 image = cv2.imread("")
-#cla
+
+#background
+white_screen = np.ones(window_size, np.uunt8)
 
 # video
 cap = cv2.videoCapture(0)
@@ -13,8 +20,14 @@ cap = cv2.videoCapture(0)
     
 # convert color
 
+
 # convert = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
 human_face_template = cv2.cascadeClassifier("CascadeClassifier\haarcascade_frontalface_alt.xml");
+
+if human_face_template.empty() == True:
+    pass
+
+#effects (thresholding) 
 
 
 #class
@@ -26,18 +39,30 @@ class object():
 
 
 
-#effects (thresholding)
+#scenario 1: recognize people face
+#process involved: face detection
+#using function:  text
+
+text = cv2.putText(image, text,(80,80) ,cv2.FONT_HERSHEY_SCRIPT_COMPLEX,1,)
+
+
+
+
+
 
 #window name
 #視窗名稱,變數名稱
 cv2.imshow("",)
 
+#Camera Setting
 
-
+while(True):
 # click the button esc and do the following action
-if cv2.waitKey(1) & 0xFF == ord('esc'):
-    break
+    ret, frame = cap.read(1)
+    if cv2.waitKey(1) & 0xFF == ord('esc'):
+        break
 # close the application
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
