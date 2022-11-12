85280713375355961e4434c309a8de835d0c84b3
tommyliu313 <liuchaktin@gmail.com>
2022-11-12 14:43:43 +0800

    20221112 1443

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask, request, render_template, url_for
 
-app = Flask(__name__, template_folder='app/templates')
+app = Flask(__name__, template_folder='app/templates',static_folder="app/static")
 
+@app.route('/')
+def testing():
+    return 'hello world'
+
+@app.route('/index')
+def index():
+    return render_template('page/index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error/error.html', errorstatus=404),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error/error.html', errorstatus=500),500    
+
+if __name__ == "__main__":
+    app.run(debug=True)
